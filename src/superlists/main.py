f32ff3db14550c724658bfe5489d22913471717f
elastic <elastic7327@gmail.com>
2017-05-05 19:03:53 +0900

    oh it looks more micro such things! cool

--- src/superlists/main.py
+++ src/superlists/main.py
@@ -1,16 +1,30 @@
-from flask import Flask, url_for, render_template
+from flask import Flask, url_for, render_template, session, escape, request, redirect
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return "Index Page"
+    if 'username' in session:
+        return 'Logged in as %s' % escape(session['username'])
+    else:
+        return 'You are not logged in \n ps: I KNOW WHO YOU ARE %s' % (session.get('username','Daniel'))
 
 @app.route('/login', methods=["GET", "POST"])
 def login():
     if request.method == "POST":
-        do_the_login()
+        session['username'] = request.form['username']
+        return redirect(url_for('index'))
     else:
-        show_the_login_form()
+        return '''
+            <form action='' method='POST'>
+                <p><input type=text name=username></p>
+                <p><input type=submit value=Login></p>
+            <form>
+        '''
+
+@app.route('/logout', methods=["GET"])
+def logout():
+    session.pop('username', None)
+    return redirect(url_for('index'))
 
 @app.route('/hello')
 @app.route('/hello/<name>')
@@ -36,11 +50,13 @@ def about():
     return 'The projects page'
 
 
-with app.test_request_context():
-    print url_for('index')
-    print url_for('login')
-    print url_for('login', next='/')
-    print url_for('profile', username='DANIEL KIM')
+#with app.test_request_context():
+#    print url_for('index')
+#    print url_for('login')
+#    print url_for('login', next='/')
+#    print url_for('profile', username='DANIEL KIM')
+
+app.secret_key = 'AMNAS@#(@#9===ASKDj)KAS=/ASDKK(@(#KDASDJKAAAL)@(I@#U))'
 
 if __name__ == "__main__":
     app.run(debug=True)
