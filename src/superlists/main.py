06ea126d6f8ea01d7b7022fe37ff9f1f736b3297
elastic <elastic7327@gmail.com>
2017-05-05 17:01:10 +0900

    Hola

--- src/superlists/main.py
+++ src/superlists/main.py
@@ -1,16 +1,23 @@
-from flask import Flask
+from flask import Flask, url_for
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     return "Index Page"
 
+@app.route('/login', methods=["GET", "POST"])
+def login():
+    if request.method == "POST":
+        do_the_login()
+    else:
+        show_the_login_form()
+
 @app.route('/hello')
 def hello_world():
     return "Hello Flask"
 
 @app.route('/user/<username>')
-def show_user_profile(username):
+def profile(username):
     # show the user profile for that user
     return 'User %s' % username
 
@@ -24,8 +31,16 @@ def projects():
     return 'The projects page'
 
 @app.route('/about')
-def projects():
+def about():
     return 'The projects page'
+
+
+with app.test_request_context():
+    print url_for('index')
+    print url_for('login')
+    print url_for('login', next='/')
+    print url_for('profile', username='DANIEL KIM')
+
 
 
 if __name__ == "__main__":
